a4da413910fefc6f726c6ea212830eaab498a772
tankyskey <tanksykey@gmail.com>
2021-04-10 18:47:19 +0200

    objective: 	- implement a bus network (hardcoded) following the decided pattern
    done:
    	- src/graphe/Node:
    		- initialize links
    		- new function:
    			- pulic void link(Node, String):  link two node with this orientation a-[relation]->b
    			- pulic void Dlink(Node, String): link two node with this orientation a-[relation]-b
    			- pulic void Blink(Node, String): link two node with this orientation a<-[relation]-b
    			- public static ArrayList<Node> findNodesByLabel(String): find all nodes with the label passed in parameter
    
    	- src/test/BusNet:
    		- added class
    
    	- misc:
    		- added .note file to write down changes when they append so commit comment are almost automatic
    todo:

--- src/graphe/Node.java
+++ src/graphe/Node.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 
 public class Node extends Element {
     private static ArrayList<Node> instances = new ArrayList<Node>();
-    private ArrayList<Link> links;
+    private ArrayList<Link> links = new ArrayList<Link>();
 
     public Node(String label) {
         super(label);
 
         instances.add(this);
+    }
+
+    public static Node create(String label) {
+        return new Node(label);
     }
 
     public void link(Node b, String label, int sens) {
@@ -19,8 +23,39 @@ public class Node extends Element {
         b.links.add(ln);
     }
 
+    public void link(Node b, String label) {
+        Link ln = new Link(this, b, label, Link.ATOB);
+
+        this.links.add(ln);
+        b.links.add(ln);
+    }
+
+    public void Dlink(Node b, String label) {
+        Link ln = new Link(this, b, label, Link.DUAL);
+
+        this.links.add(ln);
+        b.links.add(ln);
+    }
+
+    public void Blink(Node b, String label) {
+        Link ln = new Link(this, b, label, Link.BTOA);
+
+        this.links.add(ln);
+        b.links.add(ln);
+    }
+
     public static ArrayList<Node> getInstances() {
         return instances;
+    }
+
+    public static ArrayList<Node> findNodesByLabel(String label) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+
+        for(Node n: instances)
+            if(n.getLabel().equals(label))
+                nodes.add(n);
+
+        return nodes;
     }
 
     public boolean equals(Node b) {
